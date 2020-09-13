0f691da171aff43ef9f80eeb409996ef19c1e5f4
unknown <pkr131023@gmail.com>
2020-09-12 21:28:49 -0700

    added card file, dropdown, and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch, 
+  Route
+} from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import Homepage from './components/Homepage';
 
 class App extends Component {
   constructor(props){
@@ -16,11 +23,18 @@ class App extends Component {
 
   render() { 
     return ( 
+      <Router>
       <div className={this.state.theme ? "light page" : "dark page"}>
         <Navbar 
         theme={this.state.theme}
         switchTheme = {this.switchTheme}/>
+        <Switch>
+          <Route exact path="/">
+            <Homepage theme={this.state.theme}/>
+          </Route>
+        </Switch>
       </div>
+      </Router>
      );
   }
 }
