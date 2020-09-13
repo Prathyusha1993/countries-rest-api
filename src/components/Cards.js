93206d2c7d5c98fb5c943440554775c570b36277
unknown <pkr131023@gmail.com>
2020-09-12 21:38:13 -0700

    code prettier

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,35 +1,59 @@
-import React, { Component } from 'react';
-import {
-    Link
-} from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-function Cards(props){
-    return (
-        <div>
-            {props.countries.length > 0 ?
-            <ul>
-                {props.countries.filter(country => {
-                    console.log(document.getElementById("sarch"));
-                    const regex = new RegExp(document.getElementById("search").nodeValue, 'gi');
-                    return country.name.match(regex)
-                })
-                .map(country => (
-                    <li className="card" key={country.alpha3Code}>
-                        <div className="card__image" style={{backgroundImage: `url(${country.flag})`}}></div>
-                        <div className="card__text">
-                            <Link className="card__link" to={{pathname: country.alpha3Code, state:{name:country.name}}}>{country.name}</Link>
-                            <ul>
-                                <li className="card__properties"><span className="card__bold">Population: </span>{country.population.toLocaleString('en-US')}</li>
-                                <li className="card__properties"><span className="card__bold">Region: </span>{country.region}</li>
-                                <li className="card__properties"><span className="card__bold">Capital: </span>{country.capital}</li>
-                            </ul>
-                        </div>
+function Cards(props) {
+  return (
+    <div>
+      {props.countries.length > 0 ? (
+        <ul>
+          {props.countries
+            .filter((country) => {
+              console.log(document.getElementById("sarch"));
+              const regex = new RegExp(
+                document.getElementById("search").nodeValue,
+                "gi"
+              );
+              return country.name.match(regex);
+            })
+            .map((country) => (
+              <li className="card" key={country.alpha3Code}>
+                <div
+                  className="card__image"
+                  style={{ backgroundImage: `url(${country.flag})` }}
+                ></div>
+                <div className="card__text">
+                  <Link
+                    className="card__link"
+                    to={{
+                      pathname: country.alpha3Code,
+                      state: { name: country.name },
+                    }}
+                  >
+                    {country.name}
+                  </Link>
+                  <ul>
+                    <li className="card__properties">
+                      <span className="card__bold">Population: </span>
+                      {country.population.toLocaleString("en-US")}
                     </li>
-                ))}
-            </ul>
-            : <div>No countries found </div>}
-        </div>
-    )
+                    <li className="card__properties">
+                      <span className="card__bold">Region: </span>
+                      {country.region}
+                    </li>
+                    <li className="card__properties">
+                      <span className="card__bold">Capital: </span>
+                      {country.capital}
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            ))}
+        </ul>
+      ) : (
+        <div>No countries found </div>
+      )}
+    </div>
+  );
 }
 
 export default Cards;
